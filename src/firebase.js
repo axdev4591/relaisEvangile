0ef5675d3f34e1f570d335e222b7e33ade1f488e
Axdev <axelmouele@gmail.com>
2020-10-06 13:00:54 +0200

    firts commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA1maM5GGsAd461IA9D7Ffdf5JbRhiihbg",
+    authDomain: "relaisdevangile-7da64.firebaseapp.com",
+    databaseURL: "https://relaisdevangile-7da64.firebaseio.com",
+    projectId: "relaisdevangile-7da64",
+    storageBucket: "relaisdevangile-7da64.appspot.com",
+    messagingSenderId: "489098935202",
+    appId: "1:489098935202:web:de1c38722806c2245a10fb",
+    measurementId: "G-WG8XBLP244"
+  };
